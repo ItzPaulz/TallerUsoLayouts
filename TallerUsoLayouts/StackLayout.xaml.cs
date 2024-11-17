3e57b7cd0e5afa2cbeb5bb132c2ecfe54678612e
roberto.guaña <robertofelipe20.rf@gmail.com>
2024-11-16 22:15:27 -0500

    PAGINA STACKLAYOUT TERMINADA

--- TallerUsoLayouts/StackLayout.xaml.cs
+++ TallerUsoLayouts/StackLayout.xaml.cs
@@ -1,9 +1,17 @@
+
+
 namespace TallerUsoLayouts;
 
 public partial class StackLayout : ContentPage
 {
-	public StackLayout()
-	{
-		InitializeComponent();
-	}
+
+    public StackLayout()
+    {
+        InitializeComponent();
+    }
+
+    private async void BotonPage2_Clicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new FlexLayout());
+    }
 }
