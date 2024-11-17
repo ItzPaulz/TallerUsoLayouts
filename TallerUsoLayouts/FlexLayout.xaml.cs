b8f4e102c5767384965007546d32332c8fa1bf43
roberto.guaña <robertofelipe20.rf@gmail.com>
2024-11-16 22:53:59 -0500

    PAGINA 3 - TERMINADO

--- TallerUsoLayouts/FlexLayout.xaml.cs
+++ TallerUsoLayouts/FlexLayout.xaml.cs
@@ -1,9 +1,18 @@
+
+
 namespace TallerUsoLayouts;
 
 public partial class FlexLayout : ContentPage
 {
-	public FlexLayout()
-	{
-		InitializeComponent();
-	}
+    public FlexLayout()
+    {
+        InitializeComponent();
+    }
+
+    private async void OnImageTapped(object sender, EventArgs e)
+    {
+
+        await Navigation.PushAsync(new AbsoluteLayout());
+    }
+
 }
